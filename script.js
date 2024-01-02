63dc824046c33ec2a8b37ea69b8fd7373ea49865
Nonexdog <63328613+Nonexdog@users.noreply.github.com>
2024-01-02 11:02:58 -0300

    Update algorithm to hone out the logic some more

--- script.js
+++ script.js
@@ -38,3 +38,6 @@ buttonsCalc.forEach(button => {
   // If the next button is a number, assign to the secondNumber variable. Then: 
     // If at any point the AC button is pressed, clear everything. Highest priority. 
     // If the = button or another arithmetic button is pressed, then return the computed value. In the latter case, assume that this is the new operator variable. 
+
+// To do this, I will create a global display variable and a function that is called at every event click. 
+// This function will read the clicked argument and assign it to the firstNumber, operator and secondNumber variables following what was established above. 
